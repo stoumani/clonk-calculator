4175315a805ba111c793b601633f9510cdd1bd9f
Salem <104225394+salemtoumani@users.noreply.github.com>
2023-02-07 08:59:50 -0500

    backbone of the UI design for the calculator using customtkinter. work in progress

---
+++ App.py
@@ -0,0 +1,25 @@
+import customtkinter
+
+from radiobuttonframe import RadioButtonFrame
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry("1000x500")
+        self.title("Multiso")
+
+        self.radio_button_frame_1 = RadioButtonFrame(self, header_name="RadioButtonFrame 1")
+        self.radio_button_frame_1.grid(row=0, column=0, padx=20, pady=20)
+
+        self.frame_1_button = customtkinter.CTkButton(self, text="Confirm", command=self.print_value_frame_1)
+        self.frame_1_button.grid(row=1, column=0, padx=20, pady=10)
+
+    def print_value_frame_1(self):
+        print(f"Frame 1 value: {self.radio_button_frame_1.get_value()}")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
