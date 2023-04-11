38cde456cda009c367fd19292fdf710d3b2def45
Salem <104225394+salemtoumani@users.noreply.github.com>
2023-04-11 16:46:41 -0400

    structure changes

---
+++ setup/Main.py
@@ -0,0 +1,9 @@
+from tkinter import BOTTOM, BOTH, TOP, Label, DISABLED, NORMAL, INSERT, END, messagebox, ttk
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
+import Buttons
+import Windows
+from numpy import *
+import math
+
+
+
