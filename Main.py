e118f50ed46ea1892bc3fb6a1b84e5e17ff627f5
Salem <104225394+salemtoumani@users.noreply.github.com>
2023-04-11 16:44:29 -0400

    put buttons, frames and windows.py into the setup folder

--- Main.py
+++ Main.py
@@ -1,9 +1,4 @@
-from tkinter import BOTTOM, BOTH, TOP, Label, DISABLED, NORMAL, INSERT, END, messagebox, ttk
-from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
-import Buttons
-import Windows
-from numpy import *
-import math
+
 
 
 
